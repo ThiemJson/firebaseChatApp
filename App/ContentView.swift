c5ba67a9c46fb22b80753e3f3e53687478b255b4
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-01-04 10:44:51 +0700

    Initial Commit

---
+++ App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by Teneocto on 1/4/21.
+//  Copyright © 2021 NguyenCaoThiem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
