f7558948a8c01bebb9268f03c036fefcfa041dff
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-01-04 11:24:05 +0700

    Init

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -10,12 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        FirstPage()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct FirstPage : View {
+    @State private var no = ""
+    var body: some View {
+        VStack(spacing: 20){
+            Image("pic")
+            Text ("Verify Your Phone Number")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Text("Enter your phone number to verify your account")
+                .font(.body)
+                .foregroundColor(.gray)
+                .padding(.top, 12)
+            TextField("Number", text: $no)
+                .padding()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button(action : {
+                
+            }){
+                Text("Send")
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.orange)
+                
+            }
+        }
     }
 }
